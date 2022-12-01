06487d1bd63401493a75f3939435257454828979
Anna Molodtsova <renasafetysea@mail.ru>
2022-12-01 20:08:20 +0300

    сделано

---
+++ src/main/java/com/skypro/calculator/calculator/exception/IllegalNumberException.java
@@ -0,0 +1,8 @@
+package com.skypro.calculator.calculator.exception;
+
+public class IllegalNumberException extends RuntimeException{
+
+    public IllegalNumberException(String message) {
+        super(message);
+    }
+}
